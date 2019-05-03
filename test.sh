845811e992d634033cd5910c66bd4ccd062a18c9
FederAndInk <federandink@gmail.com>
2019-05-03 18:31:17 +0200

    better test script, take in account return values

--- test.sh
+++ test.sh
@@ -1,13 +1,49 @@
 #!/usr/bin/env bash
 
 javac **/*.java
+ret=$?
+
+nbTest=0
+nbTestPassed=0
+
 if (($# == 1)); then
   java test/$1
+  lastRet=$?
+  ret=$(($ret || $lastRet))
+  nbTest=$((nbTest + 1))
+  if (($lastRet)); then
+    printf "$test: \e[32mSuccess\e[0m\n"
+    nbTestPassed=$((nbTestPassed + 1))
+  else
+    printf "$test: \e[31mFailure\e[0m\n"
+  fi
 else
   for test in test/*.class; do
+    echo "-------------------------------------------"
     echo "$test"
+    echo "-------------------------------------------"
     java ${test%.class}
+    lastRet=$?
+    ret=$(($ret || $lastRet))
+    nbTest=$((nbTest + 1))
+    if (($lastRet == 0)); then
+      printf "$test: \e[32mSuccess\e[0m\n"
+      nbTestPassed=$((nbTestPassed + 1))
+    else
+      printf "$test: \e[31mFailure($lastRet)\e[0m\n"
+    fi
+    echo
   done
 fi
 
 rm -r **/*.class
+
+echo "~~~~~~~~~~~~~~~~Result~~~~~~~~~~~~~~~~"
+if (($nbTest == $nbTestPassed)); then
+  printf "\e[32mSuccess\e[0m\n"
+else
+  printf "\e[31mFailure\e[0m\n"
+fi
+printf "\e[35;1m$nbTestPassed tests passed out of $nbTest\e[0m\n"
+
+exit $ret
