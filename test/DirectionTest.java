0bc3fe95853e3dffc7bd4375db128642132bf734
FederAndInk <federandink@gmail.com>
2019-05-04 16:43:51 +0200

    Test class, test piece state change consistency

--- test/DirectionTest.java
+++ test/DirectionTest.java
@@ -1,40 +1,26 @@
 package test;
 
 import model.Direction;
+import test.tools.Test;
 
 /**
  * DirectionTest
  */
 public class DirectionTest {
-  int nbTest = 0;
-  int nbTestPassed = 0;
-
-  void test(boolean b, String msg) {
-    nbTest++;
-    if (!b) {
-      System.err.println(msg);
-    } else {
-      nbTestPassed++;
-    }
-  }
+  Test t = new Test("DirectionTest");
 
   public DirectionTest() {
     Direction d = Direction.UP;
-    test(d.right() == Direction.RIGHT, "UP.right()");
-    test(d.right().right() == Direction.DOWN, "UP.right().right()");
-    test(d.right().right().right() == Direction.LEFT, "UP.right().right().right()");
-    test(d.right().right().right().right() == Direction.UP, "UP.right().right().right().right()");
+    t.test(d.right() == Direction.RIGHT, "UP.right()");
+    t.test(d.right().right() == Direction.DOWN, "UP.right().right()");
+    t.test(d.right().right().right() == Direction.LEFT, "UP.right().right().right()");
+    t.test(d.right().right().right().right() == Direction.UP, "UP.right().right().right().right()");
 
-    test(d.left() == Direction.LEFT, "UP.left()");
-    test(d.left().left() == Direction.DOWN, "UP.left().left()");
-    test(d.left().left().left() == Direction.RIGHT, "UP.left().left().left()");
-    test(d.left().left().left().left() == Direction.UP, "UP.left().left().left().left()");
-
-    System.out.println("DirectionTest results: ");
-    System.out.println(nbTestPassed + "/" + nbTest + " tests passed");
-    if (nbTest != nbTestPassed) {
-      System.exit(1);
-    }
+    t.test(d.left() == Direction.LEFT, "UP.left()");
+    t.test(d.left().left() == Direction.DOWN, "UP.left().left()");
+    t.test(d.left().left().left() == Direction.RIGHT, "UP.left().left().left()");
+    t.test(d.left().left().left().left() == Direction.UP, "UP.left().left().left().left()");
+    t.end();
   }
 
   public static void main(String[] args) {
--- test/PieceTest.java
+++ test/PieceTest.java
@@ -1,11 +1,14 @@
 package test;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 
 import model.Config;
 import model.Coord;
+import model.Direction;
 import model.Piece;
 import model.PieceReader;
+import test.tools.Test;
 
 /**
  * PieceTest
@@ -39,8 +42,60 @@ public class PieceTest {
     InputStream is = Config.loadRsc("pieces");
     PieceReader pr = new PieceReader(is);
 
+    ArrayList<Piece> ps = new ArrayList<>();
     while ((p = pr.nextPiece()) != null) {
+      ps.add(p);
       System.out.println(p);
     }
+    p = ps.get(12);
+    Piece pRef = new Piece(p);
+
+    Test t1 = new Test("Piece manip");
+
+    System.out.println(p);
+    t1.test(p.getDirection() == Direction.UP, "piece should be UP by default");
+    t1.test(!p.isReverted(), "piece should not be reverted by default");
+
+    p.left();
+    Piece pRefLeft = new Piece(p);
+    System.out.println(p);
+    t1.test(p.getDirection() == Direction.LEFT, "piece should be LEFT after one left()");
+    t1.test(!p.isReverted(), "piece should not be reverted after one left()");
+
+    p.left();
+    System.out.println(p);
+    t1.test(p.getDirection() == Direction.DOWN, "piece should be DOWN after two left()");
+    t1.test(!p.isReverted(), "piece should not be reverted after two left()");
+
+    p.left();
+    System.out.println(p);
+    t1.test(p.getDirection() == Direction.RIGHT, "piece should be RIGHT after three left()");
+    t1.test(!p.isReverted(), "piece should not be reverted after three left()");
+
+    p.left();
+    System.out.println(p);
+    t1.test(p.equals(pRef), "piece should be equals to refence piece taken before any modif");
+    t1.test(p.getDirection() == Direction.UP, "piece should be UP after four left()");
+    t1.test(!p.isReverted(), "piece should not be reverted after four left()");
+
+    p.right();
+    // p should be right
+    System.out.println(p);
+    t1.test(p.getDirection() == Direction.RIGHT, "piece should be RIGHT after four left() then one right()");
+
+    p.revertX();
+    // p should still be right but reverted
+    System.out.println(p);
+    t1.test(p.getDirection() == Direction.RIGHT, "piece should STILL be RIGHT after one right() then revertX()");
+    t1.test(p.isReverted(), "piece should be reverted after one revert_() eg: revertX()");
+
+    p.revertY();
+    // p should LEFT
+    System.out.println(p);
+    t1.test(p.equals(pRefLeft), "piece should be equals to refence piece taken on left()");
+    t1.test(p.getDirection() == Direction.LEFT, "piece should be LEFT after one right() then revertY()");
+    t1.test(!p.isReverted(), "piece should NOT be reverted after two revert_() eg: revertX() then revertY()");
+
+    t1.end();
   }
 }
