c29f3e82b5e53b4896647944a869a2038d56c3e1
BlackDaphnir <theo.cuoq@gmail.com>
2019-05-29 13:02:25 +0200

    Started tests for board

--- src/test/java/blokus/model/TestBoard.java
+++ src/test/java/blokus/model/TestBoard.java
@@ -24,56 +24,82 @@ public class TestBoard {
 
     @BeforeClass
     public void init() {
-        pieces = loadFile("piecesLEFT_REVERTED");
+        pieces = loadFile("piecesDOWN_REVERTED");
     }
 
     @Test
-    public void test_canAdd(GameType gt) { // Test for add/getAccCorners as well?
+    public void test_canAdd() { // Test for add/getAccCorners as well?
         // Try some coherente addings (each corner/ + a Piece bordering to an other but
         // with different colors)
-        Board b = new Board(gt);
-        for (int i = 0; i < 4; i++) {
-            Iterator<Coord> it = b.getAccCorners(PColor.get((byte) i)).iterator();
-            if (it.hasNext()) {
-                assertTrue(b.canAdd(pieces.get(0), it.next(), PColor.get((byte) i)));
-                b.add(pieces.get(0), it.next(), PColor.get((byte) i));
-            }
-        }
-        switch (gt) {
-        case BLOKUS:
-            b.add(pieces.get(14), new Coord(1, b.getSize() - 2), b.get(new Coord(0, b.getSize() - 1)));
-            b.add(pieces.get(14), new Coord(6, b.getSize() - 1), b.get(new Coord(0, b.getSize() - 1)));
-            b.add(pieces.get(14), new Coord(15, b.getSize() - 3), b.get(new Coord(b.getSize() - 1, b.getSize() - 1)));
-            assertTrue(b.canAdd(pieces.get(14), new Coord(10, b.getSize() - 2),
-                    b.get(new Coord(b.getSize() - 1, b.getSize() - 1))));
-            // Try canAdd on a border of an other piece of the same color
-            assertFalse(b.canAdd(pieces.get(4), new Coord(7, b.getSize() - 2), b.get(new Coord(0, b.getSize() - 1))));
-            // Try canAdd without color coherence
-            assertFalse(b.canAdd(pieces.get(0), new Coord(14, b.getSize() - 4), PColor.get((byte) 0)));
-            // b.add(pieces.get(14), new Coord(10,b.getSize() -2), b.get(new
-            // Coord(b.getSize() -1, b.getSize()-1))); possible test de add apres le canAdd
-            // correspondant
-            break;
-        case DUO: // TODO
-            break;
-        }
+        // -----------------------------------------BLOKUS-----------------------------------------//
+        Board b = new Board(GameType.BLOKUS);
+
         // Try canAdd on a non-corner square
-        b = new Board(gt);
-        assertFalse(b.canAdd(pieces.get(0), new Coord(1, 1), PColor.get((byte) 0)));
+        assertFalse(b.canAdd(pieces.get(0), new Coord(1, 1), PColor.RED));
+
+        // Add 1x1 piece in each corner
+        assertTrue(b.canAdd(pieces.get(0), new Coord(0, 0), PColor.BLUE));
+        b.add(pieces.get(0), new Coord(0, 0), PColor.BLUE);
+
+        assertTrue(b.canAdd(pieces.get(0), new Coord(b.getSize() - 1, b.getSize() - 1), PColor.YELLOW));
+        b.add(pieces.get(0), new Coord(b.getSize() - 1, b.getSize() - 1), PColor.YELLOW);
+
+        assertTrue(b.canAdd(pieces.get(0), new Coord(0, b.getSize() - 1), PColor.GREEN));
+        b.add(pieces.get(0), new Coord(0, b.getSize() - 1), PColor.GREEN);
+
+        assertTrue(b.canAdd(pieces.get(0), new Coord(b.getSize() - 1, 0), PColor.RED));
+        b.add(pieces.get(0), new Coord(b.getSize() - 1, 0), PColor.RED);
+
+        // Adding a few pieces of another colour for the next test
+        b.add(pieces.get(14), new Coord(1, b.getSize() - 2), PColor.GREEN);
+        b.add(pieces.get(14), new Coord(6, b.getSize() - 1), PColor.GREEN);
+        b.add(pieces.get(14), new Coord(15, b.getSize() - 3), PColor.YELLOW);
+
+        // Try canAdd on a border of an other piece of a different color
+        assertTrue(b.canAdd(pieces.get(14), new Coord(10, b.getSize() - 2), PColor.YELLOW));
+        // Try canAdd on a border of an other piece of the same color
+        assertFalse(b.canAdd(pieces.get(4), new Coord(7, b.getSize() - 2), PColor.GREEN));
+
+        // Try canAdd without color coherence
+        assertFalse(b.canAdd(pieces.get(0), new Coord(14, b.getSize() - 4), PColor.YELLOW));
+        // b.add(pieces.get(14), new Coord(10,b.getSize() -2), b.get(new
+        // Coord(b.getSize() -1, b.getSize()-1))); possible test de add apres le canAdd
+        // correspondant
+
+        b = new Board(GameType.BLOKUS);
+
+        b.add(pieces.get(5), new Coord(0, 0), PColor.BLUE);
+        b.add(pieces.get(7), new Coord(2, 2), PColor.BLUE);
+        assertFalse(b.canAdd(pieces.get(0), new Coord(2, 2), PColor.BLUE));
+        b.add(pieces.get(0), new Coord(b.getSize() - 1, b.getSize() - 1), PColor.YELLOW);
+        b.add(pieces.get(6), new Coord(b.getSize() - 4, 0), PColor.RED);
+        b.add(pieces.get(14), new Coord(11, 1), PColor.RED);
+        b.add(pieces.get(14), new Coord(6, 0), PColor.RED);
+        assertFalse(b.canAdd(pieces.get(4), new Coord(5, 1), PColor.RED));
+        // Try canAdd with an exit of the board
+
+        // -------------------------------------------DUO-------------------------------------------------//
+        b = new Board(GameType.DUO);
+        b.add(pieces.get(0), new Coord(4, 4), PColor.BLUE);
+        b.add(pieces.get(0), new Coord(b.getSize() - 5, b.getSize() - 5), PColor.YELLOW);
+
+        // Try canAdd on a non-starting square
+        assertFalse(b.canAdd(pieces.get(0), new Coord(1, 1), PColor.BLUE));
+
+        // Try canAdd on a border of an other piece of a different color
+        b.add(pieces.get(13), new Coord(5, 5), PColor.BLUE);
+        b.add(pieces.get(2), new Coord(7, 8), PColor.BLUE);
+        assertTrue(b.canAdd(pieces.get(9), new Coord(b.getSize() - 7, b.getSize() - 4), PColor.YELLOW));
+        // Try canAdd on a border of an other piece of the same color
+        assertFalse(b.canAdd(pieces.get(0), new Coord(b.getSize() - 5, b.getSize() - 5), PColor.YELLOW));
+        // Try canAdd without color coherence
+        assertFalse(b.canAdd(pieces.get(0), new Coord(3, 3), PColor.YELLOW));
+
         // Try canAdd on an occupied square
-        switch (gt) {
-        case BLOKUS:
-            b.add(pieces.get(5), new Coord(0, 0), PColor.get((byte) 1));
-            b.add(pieces.get(7), new Coord(2, 3), PColor.get((byte) 1));
-            assertFalse(b.canAdd(pieces.get(0), new Coord(2, 2), PColor.get((byte) 1)));
-            b.add(pieces.get(6), new Coord(b.getSize() - 4, 0), PColor.get((byte) 0));
-            b.add(pieces.get(14), new Coord(11, 1), PColor.get((byte) 0));
-            b.add(pieces.get(14), new Coord(6, 0), PColor.get((byte) 0));
-            assertFalse(b.canAdd(pieces.get(4), new Coord(5, 1), PColor.get((byte) 0)));
-            break;
-        case DUO: // TODO
-            break;
-        }
-        // TODO : Try canAdd with an exit of the board
+        assertFalse(b.canAdd(pieces.get(0), new Coord(8, 9), PColor.BLUE));
+        assertFalse(b.canAdd(pieces.get(1), new Coord(9, 10), PColor.BLUE));
+
+        // Try canAdd with an exit of the board
+        b.canAdd(pieces.get(14), new Coord(b.getSize() - 3, b.getSize() - 3), PColor.YELLOW);
     }
 }
